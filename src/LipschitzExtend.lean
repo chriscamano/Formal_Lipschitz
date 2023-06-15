9329c63f1468a6b4c92c6f16d1f3f193131c2057
prendagast <chriscamano007@gmail.com>
2023-06-15 13:34:53 -0700

    final editsi

--- src/LipschitzExtend.lean
+++ src/LipschitzExtend.lean
@@ -24,32 +24,31 @@ theorem lipschitzWith_const [PseudoMetricSpace α] [PseudoMetricSpace β] (b: β
   intro x y; simp
 
 theorem LipschitzOnWith.extend_linf [PseudoMetricSpace α] {s : Set α} {f : α → ℓ^∞(ι)} 
-{K : ℝ≥0} (hfl : LipschitzOnWith K f s): ∃ g : α → ℓ^∞(ι), LipschitzWith K g ∧ EqOn f g s := by
-  
-  have : ∀ i : ι, ∃ g : α → ℝ, LipschitzWith K g ∧ EqOn (fun x => f x i) g s := fun i => by
-    have : LipschitzOnWith K (fun x : α => f x i) s
-    · rw [lipschitzOnWith_iff_dist_le_mul] at hfl ⊢
-      intro x hx y hy
-      calc 
-        dist (f x i) (f y i) ≤ dist (f x) (f y) := lp.norm_apply_le_norm top_ne_zero (f x - f y ) i
-        _ ≤ K * dist x y :=  hfl x hx y hy
-    exact this.extend_real
+    {K : ℝ≥0} (hfl : LipschitzOnWith K f s): ∃ g : α → ℓ^∞(ι), LipschitzWith K g ∧ EqOn f g s := by  
+  have : ∀ i : ι, ∃ g : α → ℝ, LipschitzWith K g ∧ EqOn (fun x => f x i) g s
+  · intro i
+    apply LipschitzOnWith.extend_real -- use the nonlinear Hahn-Banach theorem here!
+    rw [lipschitzOnWith_iff_dist_le_mul] at hfl ⊢
+    intro x hx y hy
+    calc 
+      dist (f x i) (f y i) ≤ dist (f x) (f y) := lp.norm_apply_le_norm top_ne_zero (f x - f y) i
+      _ ≤ K * dist x y :=  hfl x hx y hy
   choose g hg using this
   rcases s.eq_empty_or_nonempty with rfl | ⟨a₀, ha₀_in_s⟩
   . exact ⟨fun _ ↦ 0, lipschitzWith_const 0 K, by simp⟩
   · let f_ext : α → ι → ℝ := fun x i ↦ g i x
-    have hf_extb : ∀ a : α, Memℓp (f_ext a) ∞ := by 
-      intro a
+    have hf_extb : ∀ a : α, Memℓp (f_ext a) ∞
+    · intro a
       let M : ℝ := ‖f a₀‖
       use K * dist a a₀ + M
       rintro - ⟨i, rfl⟩
       rcases hg i with ⟨hgl, hgr⟩
       calc
         |g i a| = |g i a - f a₀ i + f a₀ i| := by simp
-        _ ≤ |g i a - f a₀ i| + |f a₀ i| :=  abs_add _ _ 
+        _ ≤ |g i a - f a₀ i| + |f a₀ i| := abs_add _ _ 
         _ ≤ |g i a - g i a₀| + |g i a₀ - f a₀ i| + |f a₀ i| := by gcongr; apply abs_sub_le 
-        _ = |g i a - g i a₀| + |f a₀ i| := by simp [hgr ha₀_in_s, sub_self _]
-        _ ≤ ↑K * dist a a₀ + |f a₀ i|:= by 
+        _ = |g i a - g i a₀| + |f a₀ i| := by simp [hgr ha₀_in_s]
+        _ ≤ ↑K * dist a a₀ + |f a₀ i| := by 
             gcongr
             exact lipschitzWith_iff_dist_le_mul.1 hgl a a₀
         _ ≤ ↑K * dist a a₀ + M := by
