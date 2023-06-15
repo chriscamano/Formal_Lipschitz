e1b428bbb12337d446d9fdcb142d490956d98964
prendagast <chriscamano007@gmail.com>
2023-06-15 10:59:42 -0700

    jeremy comments

--- src/LipschitzExtend.lean
+++ src/LipschitzExtend.lean
@@ -18,7 +18,7 @@ variable {α β : Type _}
 theorem lipschitzWith_const [PseudoMetricSpace α] [PseudoMetricSpace β] (b: β) (K : ℝ≥0):
   LipschitzWith K (fun _ : α ↦ b):= by
   intro x y; simp
-
+  
 theorem LipschitzOnWith.extend_linf [PseudoMetricSpace α] {s : Set α} {f : α → ℓ^∞(ι)} 
 {K : ℝ≥0} (hfl : LipschitzOnWith K f s): ∃ g : α → ℓ^∞(ι), LipschitzWith K g ∧ EqOn f g s := by
   
@@ -33,7 +33,7 @@ theorem LipschitzOnWith.extend_linf [PseudoMetricSpace α] {s : Set α} {f : α 
     exact this.extend_real
   choose g hg using this
   rcases s.eq_empty_or_nonempty with rfl | ⟨a₀, ha₀_in_s⟩
-  . use fun _↦ 0, lipschitzWith_const 0 K
+  . use fun _ ↦ 0, lipschitzWith_const 0 K
     simp
   · let f_ext : α → ι → ℝ := fun x i => g i x --<-------- golf me?
     have hf_extb : ∀ a : α, Memℓp (f_ext a) ∞ := by 
@@ -56,7 +56,7 @@ theorem LipschitzOnWith.extend_linf [PseudoMetricSpace α] {s : Set α} {f : α 
           simp_rw [hgr, sub_self _]
         _ ≤ ↑K * dist a a₀ + abs (f a₀ i):= by 
             gcongr
-            rw[lipschitzWith_iff_dist_le_mul] at hgl
+            rw [lipschitzWith_iff_dist_le_mul] at hgl
             exact hgl a a₀ 
         _ ≤ ↑K * dist a a₀ + M := by
             gcongr    
@@ -64,12 +64,12 @@ theorem LipschitzOnWith.extend_linf [PseudoMetricSpace α] {s : Set α} {f : α 
             apply lp.norm_apply_le_norm top_ne_zero   
     let f_ext' : α → ℓ^∞(ι) := fun i ↦ ⟨f_ext i, hf_extb i⟩
     refine ⟨f_ext', ?_, ?_⟩
-    · rw[lipschitzWith_iff_dist_le_mul]
+    · rw [lipschitzWith_iff_dist_le_mul]
       intro x y 
       apply lp.norm_le_of_forall_le; positivity
       intro i 
       have hgi:= (hg i).1
-      rw[lipschitzWith_iff_dist_le_mul] at hgi
+      rw [lipschitzWith_iff_dist_le_mul] at hgi
       exact hgi x y
     · intro a hyp
       ext i --<-------- golf me
