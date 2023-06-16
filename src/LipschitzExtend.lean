cdd34f7edeb2470e796f2c7c1b560957c310aa05
prendagast <chriscamano007@gmail.com>
2023-06-16 16:17:32 -0700

    current PR for mathlibi

--- src/LipschitzExtend.lean
+++ src/LipschitzExtend.lean
@@ -1,3 +1,7 @@
+/-
+Copyright (c) 2023 Chris Camaño, Ian Bunner
+Authors: Chris Camaño, Ian Bunner
+-/
 import Mathlib.Topology.MetricSpace.Lipschitz 
 import Mathlib.Analysis.NormedSpace.lpSpace
 import Mathlib.Data.Real.ENNReal
@@ -5,7 +9,7 @@ import Mathlib.Data.Set.Function
 import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.Algebra.Group.Defs
 
-open  ENNReal Metric Function Set 
+open  ENNReal Metric Function Set Function
 open scoped NNReal BigOperators Group
 
 @[app_unexpander abs] def unexpandAbs : Lean.PrettyPrinter.Unexpander
@@ -15,51 +19,77 @@ open scoped NNReal BigOperators Group
 /- A function `f : α → (ι → ℝ)` which is `K`-Lipschitz on a subset `s` admits a `K`-Lipschitz
 extension to the whole space. -/
 
-notation "ℓ^∞(" ι ") " => lp (fun i : ι => ℝ ) ∞
+local notation "ℓ^∞(" ι ")" => lp (fun i : ι => ℝ) ∞
 
 variable {α β : Type _}
+lemma LipschitzWith.uniformly_bounded [PseudoMetricSpace α] (g : α → ι → ℝ) {K : ℝ≥0}
+    (hg : ∀ i, LipschitzWith K (g · i)) (a₀ : α) (hga₀b : Memℓp (g a₀) ∞) (a : α) :
+    Memℓp (g a) ∞ := by
+  rcases hga₀b with ⟨M, hM⟩
+  use ↑K * dist a a₀ + M
+  rintro - ⟨i, rfl⟩
+  calc
+    |g a i| = |g a i - g a₀ i + g a₀ i| := by simp
+    _ ≤ |g a i - g a₀ i| + |g a₀ i| := abs_add _ _
+    _ ≤ ↑K * dist a a₀ + M := by
+        gcongr
+        . exact lipschitzWith_iff_dist_le_mul.1 (hg i) a a₀
+        . exact hM ⟨i, rfl⟩
 
-theorem lipschitzWith_const [PseudoMetricSpace α] [PseudoMetricSpace β] (b: β) (K : ℝ≥0):
-  LipschitzWith K (fun _ : α ↦ b):= by
-  intro x y; simp
+theorem LipschitzOnWith.coordinate [PseudoMetricSpace α] (f : α → ℓ^∞(ι)) (s : Set α) (K : ℝ≥0) :
+    LipschitzOnWith K f s ↔ ∀ i : ι, LipschitzOnWith K (fun a : α ↦ f a i) s := by
+  simp_rw [lipschitzOnWith_iff_dist_le_mul]
+  constructor
+  · intro hfl i x hx y hy
+    calc
+      dist (f x i) (f y i) ≤ dist (f x) (f y) := lp.norm_apply_le_norm top_ne_zero (f x - f y) i
+      _ ≤ K * dist x y := hfl x hx y hy
+  · intro hgl x hx y hy
+    apply lp.norm_le_of_forall_le; positivity
+    intro i
+    apply hgl i x hx y hy
 
-theorem LipschitzOnWith.extend_linf [PseudoMetricSpace α] {s : Set α} {f : α → ℓ^∞(ι)} 
-    {K : ℝ≥0} (hfl : LipschitzOnWith K f s): ∃ g : α → ℓ^∞(ι), LipschitzWith K g ∧ EqOn f g s := by  
+theorem LipschitzWith.coordinate [PseudoMetricSpace α] {f : α → ℓ^∞(ι)} (K : ℝ≥0) :
+    LipschitzWith K f ↔ ∀ i : ι, LipschitzWith K (fun a : α ↦ f a i) := by
+  simp_rw [← lipschitz_on_univ]
+  apply LipschitzOnWith.coordinate
+
+theorem const'[PseudoMetricSpace α] [PseudoMetricSpace β] (b : β) {K : ℝ≥0} : LipschitzWith K fun _ : α => b := fun x y => by
+  simp only [edist_self, zero_le]
+
+/--
+A function `f : α → ℓ^∞(ι, ℝ)` which is `K`-Lipschitz on a subset `s` admits a `K`-Lipschitz
+extension to the whole space.
+
+Theorem 2.2 of [Assaf Naor, *Metric Embeddings and Lipschitz Extensions*][Naor-2015]
+
+The same result for the case of a finite type `ι` is implemented in
+`LipschitzOnWith.extend_pi`.
+-/
+theorem LipschitzOnWith.extend_lp_infty [PseudoMetricSpace α] {s : Set α} {f : α → ℓ^∞(ι)}
+    {K : ℝ≥0} (hfl : LipschitzOnWith K f s): ∃ g : α → ℓ^∞(ι), LipschitzWith K g ∧ EqOn f g s := by
+  -- Construct the coordinate-wise extensions
   have : ∀ i : ι, ∃ g : α → ℝ, LipschitzWith K g ∧ EqOn (fun x => f x i) g s
   · intro i
     apply LipschitzOnWith.extend_real -- use the nonlinear Hahn-Banach theorem here!
-    rw [lipschitzOnWith_iff_dist_le_mul] at hfl ⊢
-    intro x hx y hy
-    calc 
-      dist (f x i) (f y i) ≤ dist (f x) (f y) := lp.norm_apply_le_norm top_ne_zero (f x - f y) i
-      _ ≤ K * dist x y :=  hfl x hx y hy
-  choose g hg using this
+    revert i
+    rw [← LipschitzOnWith.coordinate]
+    exact hfl
+  choose g hgl hgeq using this
   rcases s.eq_empty_or_nonempty with rfl | ⟨a₀, ha₀_in_s⟩
-  . exact ⟨fun _ ↦ 0, lipschitzWith_const 0 K, by simp⟩
-  · let f_ext : α → ι → ℝ := fun x i ↦ g i x
-    have hf_extb : ∀ a : α, Memℓp (f_ext a) ∞
-    · intro a
-      let M : ℝ := ‖f a₀‖
-      use K * dist a a₀ + M
+  . exact ⟨fun _ ↦ 0, const' 0, by simp⟩
+  · -- Show that the extensions are uniformly bounded
+    have hf_extb : ∀ a : α, Memℓp (swap g a) ∞
+    . apply LipschitzWith.uniformly_bounded (swap g) hgl a₀
+      use ‖f a₀‖
       rintro - ⟨i, rfl⟩
-      rcases hg i with ⟨hgl, hgr⟩
-      calc
-        |g i a| = |g i a - f a₀ i + f a₀ i| := by simp
-        _ ≤ |g i a - f a₀ i| + |f a₀ i| := abs_add _ _ 
-        _ ≤ |g i a - g i a₀| + |g i a₀ - f a₀ i| + |f a₀ i| := by gcongr; apply abs_sub_le 
-        _ = |g i a - g i a₀| + |f a₀ i| := by simp [hgr ha₀_in_s]
-        _ ≤ ↑K * dist a a₀ + |f a₀ i| := by 
-            gcongr
-            exact lipschitzWith_iff_dist_le_mul.1 hgl a a₀
-        _ ≤ ↑K * dist a a₀ + M := by
-            gcongr
-            exact lp.norm_apply_le_norm top_ne_zero (f a₀) i
-    let f_ext' : α → ℓ^∞(ι) := fun i ↦ ⟨f_ext i, hf_extb i⟩
+      simp_rw [←hgeq i ha₀_in_s]
+      exact lp.norm_apply_le_norm top_ne_zero (f a₀) i
+    -- Construct witness by bundling the function with its certificate of membership in ℓ^∞
+    let f_ext' : α → ℓ^∞(ι) := fun i ↦ ⟨swap g i, hf_extb i⟩
     refine ⟨f_ext', ?_, ?_⟩
-    · rw [lipschitzWith_iff_dist_le_mul]
-      intro x y 
-      apply lp.norm_le_of_forall_le; positivity
-      exact fun i ↦ lipschitzWith_iff_dist_le_mul.mp (hg i).1 x y
+    · rw [LipschitzWith.coordinate]
+      exact hgl
     · intro a hyp
-      ext i 
-      exact (hg i).2 hyp 
+      ext i
+      exact (hgeq i) hyp
